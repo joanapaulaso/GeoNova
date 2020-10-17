6beefcb601cf2990aabdb35c7a53e81b9cc334ec
joanapaulaso <joanapaulasoliveira@gmail.com>
2020-10-17 08:38:14 -0300

    Version 1.0 17.10.2020
    Versão inicial do site GeoNova, ainda em construção.

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GeoNova</title>
+
+    <link rel = "stylesheet" type="text/css" href = "CSS/background.css">
+    <link rel ="stylesheet" type="text/css" href="CSS/custom-responsive-style.css">
+    <link rel = "stylesheet" type="text/css" href = "CSS/imgload.css">
+    <link rel = "stylesheet" type="text/css" href = "CSS/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+
+    <div class="social-medias">
+
+        <table class="upper-table">
+                <tbody>
+                    <tr>
+                        <td class="logos-td"><img src="IMG/facebook-logo.png" class="logos"></td>
+                        <td class="logos-td"><img src="IMG/instagram-logo.png" class="logos"></td>
+                        <td class="logos-td"><img src="IMG/linkedin-logo.png" class="logos"></td>
+                    </tr>
+                </tbody>
+            </table>
+
+    </div>
+
+    <div class="upper-bar"></div>
+
+    <div class="logo-bar">
+
+        <img src="IMG/logo_branco.png" alt="logo_branco" class="logo"><br>
+        <h1 class="subtitulo">
+            Levantamento Planialtimétrico e Mapeamento Geoambiental
+        </h1>
+
+    </div>
+
+    <div class="about-bar">
+
+        <div class="bar-in">
+
+            <p class="section-header">Sobre nós</p>
+            <p class="section-text">
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas cursus nibh sapien, quis dictum augue dapibus eu. Ut mattis porta sem, at aliquam nisl feugiat ut. Nam sit amet augue facilisis neque tempus rutrum. Proin suscipit dignissim arcu pulvinar tincidunt. Phasellus ipsum ante, ultricies et ornare sit amet, suscipit viverra neque. Curabitur egestas, quam sit amet porttitor fermentum, libero leo dignissim arcu, nec tristique purus ligula at purus. Integer vel libero ex. Curabitur sed congue nunc, id sagittis magna. Proin sodales tristique quam. Mauris non nunc eget est malesuada pharetra condimentum vel elit. Proin efficitur risus eu ligula vestibulum sodales. Vestibulum ut nulla viverra, placerat metus nec, blandit dolor. Vestibulum ipsum diam, egestas et ultricies at, luctus quis ante. Pellentesque condimentum, felis ut ultricies pellentesque, lorem ligula pretium urna, vitae euismod massa ex eget libero. Suspendisse egestas quam diam, in feugiat sapien finibus sed. Nam et ipsum in leo ultricies consectetur ut at augue. Morbi quis dolor quis turpis cursus ultrices. Sed lobortis vulputate libero non laoreet. Nulla rutrum felis orci, eget ultricies enim iaculis nec. Donec facilisis est vel lectus ultrices ornare. Duis a eros in lectus consequat faucibus non a magna. Ut ac elit sit amet magna dignissim posuere id id purus. Praesent pharetra mi at orci auctor, at ullamcorper metus sodales. Donec porta tincidunt orci, sed varius nisi ullamcorper ut. Aenean mollis augue ipsum, quis suscipit mauris posuere a.
+            
+            </p>
+
+        </div>
+
+    </div>
+
+    <div class="services-bar">
+
+        <div class="bar-in">
+
+            <p class="section-header">Serviços</p>
+            <p class="section-text">
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas cursus nibh sapien, quis dictum augue dapibus eu. Ut mattis porta sem, at aliquam nisl feugiat ut. Nam sit amet augue facilisis neque tempus rutrum. Proin suscipit dignissim arcu pulvinar tincidunt. Phasellus ipsum ante, ultricies et ornare sit amet, suscipit viverra neque. Curabitur egestas, quam sit amet porttitor fermentum, libero leo dignissim arcu, nec tristique purus ligula at purus. Integer vel libero ex. Curabitur sed congue nunc, id sagittis magna. Proin sodales tristique quam. Mauris non nunc eget est malesuada pharetra condimentum vel elit. Proin efficitur risus eu ligula vestibulum sodales. Vestibulum ut nulla viverra, placerat metus nec, blandit dolor. Vestibulum ipsum diam, egestas et ultricies at, luctus quis ante. Pellentesque condimentum, felis ut ultricies pellentesque, lorem ligula pretium urna, vitae euismod massa ex eget libero. Suspendisse egestas quam diam, in feugiat sapien finibus sed. Nam et ipsum in leo ultricies consectetur ut at augue. Morbi quis dolor quis turpis cursus ultrices. Sed lobortis vulputate libero non laoreet. Nulla rutrum felis orci, eget ultricies enim iaculis nec. Donec facilisis est vel lectus ultrices ornare. Duis a eros in lectus consequat faucibus non a magna. Ut ac elit sit amet magna dignissim posuere id id purus. Praesent pharetra mi at orci auctor, at ullamcorper metus sodales. Donec porta tincidunt orci, sed varius nisi ullamcorper ut. Aenean mollis augue ipsum, quis suscipit mauris posuere a.
+
+            </p>
+
+        </div>
+
+    </div>
+
+    <div class="equipe-bar">
+
+        <div class="bar-in">
+
+            <p class="section-header">Equipe</p>
+            <p class="section-text">
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas cursus nibh sapien, quis dictum augue dapibus eu. Ut mattis porta sem, at aliquam nisl feugiat ut. Nam sit amet augue facilisis neque tempus rutrum. Proin suscipit dignissim arcu pulvinar tincidunt. Phasellus ipsum ante, ultricies et ornare sit amet, suscipit viverra neque. Curabitur egestas, quam sit amet porttitor fermentum, libero leo dignissim arcu, nec tristique purus ligula at purus. Integer vel libero ex. Curabitur sed congue nunc, id sagittis magna. Proin sodales tristique quam. Mauris non nunc eget est malesuada pharetra condimentum vel elit. Proin efficitur risus eu ligula vestibulum sodales. Vestibulum ut nulla viverra, placerat metus nec, blandit dolor. Vestibulum ipsum diam, egestas et ultricies at, luctus quis ante. Pellentesque condimentum, felis ut ultricies pellentesque, lorem ligula pretium urna, vitae euismod massa ex eget libero. Suspendisse egestas quam diam, in feugiat sapien finibus sed. Nam et ipsum in leo ultricies consectetur ut at augue. Morbi quis dolor quis turpis cursus ultrices. Sed lobortis vulputate libero non laoreet. Nulla rutrum felis orci, eget ultricies enim iaculis nec. Donec facilisis est vel lectus ultrices ornare. Duis a eros in lectus consequat faucibus non a magna. Ut ac elit sit amet magna dignissim posuere id id purus. Praesent pharetra mi at orci auctor, at ullamcorper metus sodales. Donec porta tincidunt orci, sed varius nisi ullamcorper ut. Aenean mollis augue ipsum, quis suscipit mauris posuere a.
+
+            </p>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
+
+
